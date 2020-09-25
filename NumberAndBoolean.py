edae54abcaf21c2a19b3ff23209d3fd2508ac99e
codeasitis <71705905+codeasitis@users.noreply.github.com>
2020-09-26 03:06:53 +0530

    Uploaded Example of Number , Boolean and Sequence

---
+++ NumberAndBoolean.py
@@ -0,0 +1,14 @@
+#Integer
+result=20
+print(type(result))
+#Float
+price=23.50
+print(type(price))
+#Complex
+comp=3+5j
+print(type(comp))
+print(comp.real)
+
+#Boolean
+isBoolean=True
+print(type(isBoolean))
---
+++ Sequence.py
@@ -0,0 +1,15 @@
+# List
+mobiles=["apple","samsung","motoral"]
+print(type(mobiles))
+
+# Tuple
+fruits=("apple","banana","guava")
+print(type(fruits))
+
+# String
+fruitName="Apple"
+print(type(fruitName))
+
+# Dictionary
+workExperience={1:"Rohan",2:"Aniket",3:"Paresh"}
+print(type(workExperience))
