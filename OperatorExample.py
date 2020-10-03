b582b39b4249cb9858354fc874fefdf13625dfa5
codeasitis <71705905+codeasitis@users.noreply.github.com>
2020-10-03 19:55:16 +0530

    Added operators demo
    All the operators demo is here related to 
    1. Arithmetic operator
    2. Assignment Operator
    3. Conditional Opertaor
    4. Logical Operator
    5. Membership Operator
    6. Identity Operator
    7. Bitwise Opeartor

---
+++ OperatorExample.py
@@ -0,0 +1,29 @@
+firstNumber=10
+secondNumber=4
+print(firstNumber+secondNumber)
+print(firstNumber-secondNumber)
+print(firstNumber*secondNumber)
+print(firstNumber/secondNumber)
+print(firstNumber%secondNumber)
+print(firstNumber//secondNumber)
+
+##firstNumber//=10
+
+print(firstNumber)
+
+print(firstNumber==10)
+
+print(not(firstNumber<10))
+
+thirdNumber=[1,2,3]
+fourthNumber=[1,2,3]
+print(thirdNumber is not fourthNumber)
+
+print(4 not in  thirdNumber)
+
+print(firstNumber & secondNumber)
+print(firstNumber | secondNumber)
+print(~firstNumber)
+print(firstNumber^secondNumber)
+print(firstNumber<<2)
+print(firstNumber>>2)
