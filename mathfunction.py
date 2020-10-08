d6c21987546af95fa2db9686121c987ec000fc3d
codeasitis <71705905+codeasitis@users.noreply.github.com>
2020-10-09 01:19:52 +0530

    Add files via upload
    modules in python demo

---
+++ mathfunction.py
@@ -0,0 +1,12 @@
+firstNumber=10
+
+def sum(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+
+def printvar():
+    print(firstNumber)
+    return 
---
+++ moduleExample.py
@@ -0,0 +1,8 @@
+#import mathfunction
+from mathfunction import *
+import mathfunction as mf
+
+#mathfunction.firstNumber=30
+print(sum(10,20))
+printvar()
+print(mf.sum(20,40))
